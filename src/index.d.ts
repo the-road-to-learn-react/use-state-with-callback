b33d68c21f1ba9ff54d08e2455be0f9341c5369a
Ivan Guimarães Monte <ivg.monte@gmail.com>
2020-12-08 11:59:50 -0300

    Fixing types definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,13 @@
-import React, {SetStateAction, Dispatch} from 'react';
+import { SetStateAction, Dispatch } from 'react';
 
-type DispatchWithCallback<A> = (value: A, callback: Callback) => void;
+type DispatchWithCallback<A, S> = (value: A, callback: Callback<S>) => void;
 
-type Callback<S> = (state:s) => (void | (() => void | undefined));
+type Callback<S> = (state: S) => (void | (() => void | undefined));
 
+declare function useStateWithCallback<S>(initialState: S, callback: Callback<S>): [S, Dispatch<SetStateAction<S>>];
 
-declare function useStateWithCallback<S>(initialState:S,callback:Callback) : [S,Dispatch<SetStateAction<S>>];
+export declare function useStateWithCallbackInstant<S>(initialState: S, callback: Callback<S>): [S, Dispatch<SetStateAction<S>>];
 
-export declare function useStateWithCallbackInstant<S>(initialState:S,callback:Callback) : [S,Dispatch<SetStateAction<S>>];
-
-
-export declare function useStateWithCallbackLazy<S>(initialState:S) : [S,DispatchWithCallback<SetStateAction<S>>];
-
+export declare function useStateWithCallbackLazy<S>(initialState: S): [S, DispatchWithCallback<SetStateAction<S>, S>];
 
 export default useStateWithCallback;
